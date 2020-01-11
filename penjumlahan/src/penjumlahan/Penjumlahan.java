9243f607e599e4a71bfff7e57b81e820af1b8af5
Ganti Dengan Nama Sendiri <anggaoji@gmail.com>
2020-01-11 22:12:50 +0700

    penjumlahan berhasil ditambah

---
+++ penjumlahan/src/penjumlahan/Penjumlahan.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package penjumlahan;
+
+/**
+ *
+ * @author Angga
+ */
+public class Penjumlahan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        int a=8;
+        int b=5;
+        int tambah,bagi,kali,kurang;
+        tambah= a+b;
+        kurang=a-b;
+        kali=a*b;
+        bagi=a/b;
+        System.out.println("\n"+"nilai penjumlahan: "+tambah+"\n"+"Nilai pengurangan:"+kurang+"\n"+"Nilai perkalian:"+kali+"\n"+"Nilai pembagian:"+bagi);
+    }
+    
+}
